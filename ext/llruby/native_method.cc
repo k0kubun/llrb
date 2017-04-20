e9cda9c95f32c2c4ca2324d1e0d5d9fb47834da7
Takashi Kokubun <takashikkbn@gmail.com>
2017-04-20 21:08:13 +0900

    Try calling LLVM runtime

--- ext/llruby/native_method.cc
+++ ext/llruby/native_method.cc
@@ -1,8 +1,22 @@
 #include "llruby/native_method.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
 
 namespace llruby {
 
+static llvm::LLVMContext context;
+static llvm::IRBuilder<> builder(context);
+static std::unique_ptr<llvm::Module> module;
+
 void* NativeMethod::CreateFunction() {
+  module = llvm::make_unique<llvm::Module>("top", context);
+  llvm::Function *func = llvm::Function::Create(
+      llvm::FunctionType::get(llvm::Type::getInt32Ty(context), false),
+      llvm::Function::ExternalLinkage, "main", module.get());
+  builder.SetInsertPoint(llvm::BasicBlock::Create(context, "", func));
+  builder.CreateRet(builder.getInt32(0));
+  module->dump();
   return (void *)0;
 }
 
