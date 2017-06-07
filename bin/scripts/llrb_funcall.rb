9bcda13b03ccb1518929d675d97299d2964fd53d
Takashi Kokubun <takashikkbn@gmail.com>
2017-06-08 00:21:30 +0900

    Add method call benchmark

---
+++ bin/scripts/llrb_funcall.rb
@@ -0,0 +1,22 @@
+def func(n)
+  if 0 < n
+    func(n-1)
+    func(n-1)
+    func(n-1)
+    func(n-1)
+    func(n-1)
+    func(n-1)
+    func(n-1)
+    func(n-1)
+    func(n-1)
+    func(n-1)
+    func(n-1)
+    func(n-1)
+    func(n-1)
+    func(n-1)
+    func(n-1)
+    func(n-1)
+  end
+end
+
+func(6)
