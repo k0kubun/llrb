ac3b1904d6491fd20e083a401bf69f0debb3b00e
Takashi Kokubun <takashikkbn@gmail.com>
2017-06-11 19:22:19 +0900

    Fix warnings for C++

--- ext/llrb/extconf.rb
+++ ext/llrb/extconf.rb
@@ -1,3 +1,22 @@
 require 'mkmf'
 
+# cc1plus: warning: command line option ‘-Wimplicit-int’ is valid for C/ObjC but not for C++
+# cc1plus: warning: command line option ‘-Wdeclaration-after-statement’ is valid for C/ObjC but not for C++
+# cc1plus: warning: command line option ‘-Wimplicit-function-declaration’ is valid for C/ObjC but not for C++
+# cc1plus: warning: unrecognized command line option ‘-Wno-self-assign’
+# cc1plus: warning: unrecognized command line option ‘-Wno-constant-logical-operand’
+# cc1plus: warning: unrecognized command line option ‘-Wno-parentheses-equality’
+# cc1plus: warning: unrecognized command line option ‘-Wno-tautological-compare’
+%w[
+  -Wimplicit-int
+  -Wdeclaration-after-statement
+  -Wimplicit-function-declaration
+  -Wno-self-assign
+  -Wno-constant-logical-operand
+  -Wno-parentheses-equality
+  -Wno-tautological-compare
+].each do |flag|
+  CONFIG['warnflags'].gsub!(flag, '')
+end
+
 create_makefile('llrb/llrb')
