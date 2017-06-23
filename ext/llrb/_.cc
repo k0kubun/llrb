be3b22542997ec4ae21c9abb061fc6a263606841
Takashi Kokubun <takashikkbn@gmail.com>
2017-06-23 22:28:11 +0900

    Rename empty.cc -> _.cc

--- ext/llrb/_.cc
+++ ext/llrb/_.cc
@@ -1,4 +1,4 @@
-// This file is created to let mkmf.rb (or rake-compiler?) link with C++ linker.
+// This file is workaround to let mkmf.rb (or rake-compiler?) link with C++ linker.
 // It's required by LLVM even if we use llvm-c API.
 //
 // DO NOT WRITE ANY CODE HERE.
