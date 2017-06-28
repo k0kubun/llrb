63c201d81746efac5b557fb3f4740ed27c327aa8
Takashi Kokubun <takashikkbn@gmail.com>
2017-06-29 01:12:30 +0900

    Add InstructionCombiningPass

--- ext/llrb/optimizer.cc
+++ ext/llrb/optimizer.cc
@@ -19,10 +19,12 @@ ApplyFunctionPasses(llvm::Module *mod, llvm::Function *func)
 {
   auto fpm = llvm::make_unique<llvm::legacy::FunctionPassManager>(mod);
 
-  //fpm->add(llvm::createInstructionCombiningPass()); // SEGV with opt_newarray_min/max, throw, jump, branchif/unless
   fpm->add(llvm::createReassociatePass());
   fpm->add(llvm::createGVNPass());
-  fpm->add(llvm::createCFGSimplificationPass());
+  fpm->add(llvm::createCFGSimplificationPass()); // Removes empty basic block.
+  // This needs to be called after CFGSimplificationPass because we add empty basic block
+  // and this pass fetches terminator instruction of basic block.
+  fpm->add(llvm::createInstructionCombiningPass());
 
   fpm->doInitialization();
   fpm->run(*func);
