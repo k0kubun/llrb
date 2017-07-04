ee81d6fb52f9b4cefcf420263f8d6fbe31a0242a
Takashi Kokubun <takashikkbn@gmail.com>
2017-07-04 23:41:02 +0900

    Add some tests for catch table

--- spec/llrb/compiler/catch_table_spec.rb
+++ spec/llrb/compiler/catch_table_spec.rb
@@ -12,8 +12,20 @@ RSpec.describe 'llrb_compile_iseq' do
     GC.start # to increase possibility to hit a bug caused by GC.
   end
 
-  #specify 'break' do
-  #end
+  specify 'break' do
+    klass = Class.new
+    def klass.test1
+      test2 do
+        break 1
+      end
+    end
+    def klass.test2(&block)
+      LLRB::JIT.compile_proc(block)
+      2.tap(&block)
+    end
+
+    expect(klass.test1).to eq(1)
+  end
 
   #specify 'next' do
   #end
@@ -24,12 +36,49 @@ RSpec.describe 'llrb_compile_iseq' do
   #specify 'retry' do
   #end
 
-  #specify 'return' do
-  #end
+  specify 'return' do
+    klass = Class.new
+    def klass.test1
+      test2 do
+        return 1
+      end
+      2
+    end
+    def klass.test2(&block)
+      LLRB::JIT.compile_proc(block)
+      block.call
+      return 3
+    end
 
-  #specify 'rescue' do
-  #end
+    expect(klass.test1).to eq(1)
+  end
 
-  #specify 'ensure' do
-  #end
+  specify 'rescue' do
+    klass = Class.new
+    def klass.test
+      raise 'hello'
+    rescue RuntimeError => e
+      e
+    end
+
+    expect(LLRB::JIT.compile(klass, :test)).to eq(true)
+    expect(klass.test).to be_a(RuntimeError)
+  end
+
+  specify 'ensure' do
+    ensured = false
+    klass = Class.new
+    def klass.test
+      @ensured = false
+      raise 'hello'
+    ensure
+      @ensured = true
+    end
+
+    begin
+      klass.test
+    rescue RuntimeError
+      expect(klass.instance_variable_get(:@ensured)).to eq(true)
+    end
+  end
 end
