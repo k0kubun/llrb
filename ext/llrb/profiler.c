55ca4c5d9e421be15c24fd89b167096c60934300
Takashi Kokubun <takashikkbn@gmail.com>
2017-07-07 21:14:59 +0900

    Implement first compile scheduler

--- ext/llrb/profiler.c
+++ ext/llrb/profiler.c
@@ -1,97 +1,155 @@
-#include "ruby.h"
-#include "ruby/debug.h"
-#include "cruby.h"
+#include <stdbool.h>
 #include <signal.h>
 #include <sys/time.h>
 #include <pthread.h>
+#include "ruby.h"
+#include "ruby/debug.h"
+#include "cruby.h"
 
-#define LLRB_BUF_SIZE 2048
+#define LLRB_PROFILE_LIMIT 2048
+#define LLRB_ENQUEUE_INTERVAL 40
+
+struct llrb_sample {
+  size_t score; // Total count of callstack depth
+  bool compiled;
+  const rb_callable_method_entry_t *cme;
+};
 
 static struct {
   int running;
-  VALUE frames_buffer[LLRB_BUF_SIZE];
-  int lines_buffer[LLRB_BUF_SIZE];
+  size_t profile_times;
+  st_table *sample_by_frame; // Hash table: { frame => frame_data sample }
 } llrb_profiler;
 
-static void
-llrb_dump_iseq(const rb_iseq_t *iseq)
+static VALUE gc_hook;
+
+void
+llrb_dump_iseq(const rb_iseq_t *iseq, const struct llrb_sample *sample)
 {
-	switch (iseq->body->type) {
-	  case ISEQ_TYPE_METHOD:
-      fprintf(stderr," ISEQ_TYPE_METHOD:");
-	    break;
-	  case ISEQ_TYPE_CLASS:
-      fprintf(stderr," ISEQ_TYPE_CLASS:");
-	    break;
-	  case ISEQ_TYPE_BLOCK:
-      fprintf(stderr," ISEQ_TYPE_BLOCK:");
-	    break;
-	  case ISEQ_TYPE_EVAL:
-      fprintf(stderr," ISEQ_TYPE_EVAL:");
-	    break;
-	  case ISEQ_TYPE_MAIN:
-      fprintf(stderr," ISEQ_TYPE_MAIN:");
-	    break;
-	  case ISEQ_TYPE_TOP:
-      fprintf(stderr," ISEQ_TYPE_TOP:");
-	    break;
-	  case ISEQ_TYPE_RESCUE:
-      fprintf(stderr," ISEQ_TYPE_RESCUE:");
-	    break;
-	  case ISEQ_TYPE_ENSURE:
-      fprintf(stderr," ISEQ_TYPE_ENSURE:");
-	    break;
-	  case ISEQ_TYPE_DEFINED_GUARD:
-      fprintf(stderr," ISEQ_TYPE_DEFINED_GUARD:");
-	    break;
-	  default:
-      fprintf(stderr," default:");
+  const rb_callable_method_entry_t *cme = sample->cme;
+
+  fprintf(stderr, "%6ld: ", sample->score);
+  switch (iseq->body->type) {
+    case ISEQ_TYPE_METHOD:
+      fprintf(stderr,"ISEQ_TYPE_METHOD:");
       break;
-	}
+    case ISEQ_TYPE_CLASS:
+      fprintf(stderr,"ISEQ_TYPE_CLASS:");
+      break;
+    case ISEQ_TYPE_BLOCK:
+      fprintf(stderr,"ISEQ_TYPE_BLOCK:");
+      break;
+    case ISEQ_TYPE_EVAL:
+      fprintf(stderr,"ISEQ_TYPE_EVAL:");
+      break;
+    case ISEQ_TYPE_MAIN:
+      fprintf(stderr,"ISEQ_TYPE_MAIN:");
+      break;
+    case ISEQ_TYPE_TOP:
+      fprintf(stderr,"ISEQ_TYPE_TOP:");
+      break;
+    case ISEQ_TYPE_RESCUE:
+      fprintf(stderr,"ISEQ_TYPE_RESCUE:");
+      break;
+    case ISEQ_TYPE_ENSURE:
+      fprintf(stderr,"ISEQ_TYPE_ENSURE:");
+      break;
+    case ISEQ_TYPE_DEFINED_GUARD:
+      fprintf(stderr,"ISEQ_TYPE_DEFINED_GUARD:");
+      break;
+    default:
+      fprintf(stderr,"default:");
+      break;
+  }
+
+  if (cme && cme->def->type == VM_METHOD_TYPE_ISEQ) {
+    VALUE name = rb_profile_frame_full_label((VALUE)cme);
+    fprintf(stderr, " %s", RSTRING_PTR(name));
+  }
+  fprintf(stderr, "\n");
 }
 
-static void
-llrb_profile_iseq(const rb_iseq_t *iseq, int line)
+static struct llrb_sample *
+llrb_sample_for(const rb_iseq_t *iseq, const rb_control_frame_t *cfp)
 {
-  VALUE name = rb_profile_frame_full_label((VALUE)iseq);
-  fprintf(stderr, "iseq:");
-  llrb_dump_iseq(iseq);
-  fprintf(stderr, " %s (%d) %lx\n", RSTRING_PTR(name), line, (VALUE)iseq);
-}
+  st_data_t key = (st_data_t)iseq, val = 0;
+  struct llrb_sample *sample;
 
-static void
-llrb_profile_callable_method_entry(const rb_callable_method_entry_t *cme, int line)
-{
-  VALUE name = rb_profile_frame_full_label((VALUE)cme);
-  const rb_iseq_t *iseq = cme->def->body.iseq.iseqptr;
-  fprintf(stderr, "cme:");
-  llrb_dump_iseq(iseq);
-  fprintf(stderr, " %s (%d) %lx\n", RSTRING_PTR(name), line, (VALUE)cme);
+  if (st_lookup(llrb_profiler.sample_by_frame, key, &val)) {
+    sample = (struct llrb_sample *)val;
+  } else {
+    sample = ALLOC_N(struct llrb_sample, 1); // Not freed
+    *sample = (struct llrb_sample){
+      .score = 0,
+      .compiled = false,
+      .cme = rb_vm_frame_method_entry(cfp),
+    };
+    val = (st_data_t)sample;
+    st_insert(llrb_profiler.sample_by_frame, key, val);
+  }
+
+  return sample;
 }
 
 static void
 llrb_profile_frames()
 {
-  int num = rb_profile_frames(0, sizeof(llrb_profiler.frames_buffer) / sizeof(VALUE),
-      llrb_profiler.frames_buffer, llrb_profiler.lines_buffer);
+  rb_thread_t *th = GET_THREAD();
+  rb_control_frame_t *cfp = RUBY_VM_NEXT_CONTROL_FRAME(RUBY_VM_END_CONTROL_FRAME(th)); // reverse lookup
+  rb_control_frame_t *end_cfp = th->cfp;
 
-  fprintf(stderr, "---start---\n");
-  for (int i = 0; i < num; i++) {
-    int line = llrb_profiler.lines_buffer[i];
-    VALUE frame = llrb_profiler.frames_buffer[i];
+  for (int i = 0; i < LLRB_PROFILE_LIMIT && cfp != end_cfp; i++) {
+    if (cfp->iseq) {
+      struct llrb_sample *sample = llrb_sample_for(cfp->iseq, cfp);
+      sample->score += i;
+    }
+    cfp = RUBY_VM_NEXT_CONTROL_FRAME(cfp);
+  }
 
-    switch (imemo_type(frame)) {
-      case imemo_iseq:
-        llrb_profile_iseq((const rb_iseq_t *)frame, line);
-        break;
-      case imemo_ment:
-        llrb_profile_callable_method_entry((const rb_callable_method_entry_t *)frame, line);
-        break;
-      default:
-        break;
+  llrb_profiler.profile_times++;
+}
+
+struct llrb_compile_target {
+  const rb_iseq_t *iseq;
+  struct llrb_sample* sample;
+};
+
+static int
+llrb_search_compile_target_i(st_data_t key, st_data_t val, st_data_t arg)
+{
+  struct llrb_compile_target *target = (struct llrb_compile_target *)arg;
+  const rb_iseq_t *iseq = (const rb_iseq_t *)key;
+  struct llrb_sample *sample = (struct llrb_sample *)val;
+
+  if (sample->compiled) return ST_CONTINUE;
+
+  if (iseq->body->type == ISEQ_TYPE_METHOD || iseq->body->type == ISEQ_TYPE_BLOCK) {
+    if (!target->iseq && !target->sample) {
+      target->iseq = iseq;
+      target->sample = sample;
+    }
+    if (sample->score > target->sample->score) {
+      target->iseq = iseq;
+      target->sample = sample;
     }
   }
-  fprintf(stderr, "---end---\n");
+
+  return ST_CONTINUE;
+}
+
+static const rb_iseq_t *
+llrb_search_compile_target()
+{
+  struct llrb_compile_target target = (struct llrb_compile_target){
+    .sample = 0,
+    .iseq = 0,
+  };
+  st_foreach(llrb_profiler.sample_by_frame, llrb_search_compile_target_i, (st_data_t)&target);
+
+  if (target.sample) {
+    target.sample->compiled = true;
+  }
+  return target.iseq;
 }
 
 static void
@@ -103,6 +161,14 @@ llrb_job_handler(void *data)
 
   in_job_handler++;
   llrb_profile_frames();
+
+  if (llrb_profiler.profile_times % LLRB_ENQUEUE_INTERVAL == 0) {
+    const rb_iseq_t *target_iseq = llrb_search_compile_target();
+    st_data_t val;
+    st_lookup(llrb_profiler.sample_by_frame, (st_data_t)target_iseq, &val);
+    struct llrb_sample *sample = (struct llrb_sample *)val;
+    llrb_dump_iseq(target_iseq, sample);
+  }
   in_job_handler--;
 }
 
@@ -121,6 +187,9 @@ rb_jit_start(RB_UNUSED_VAR(VALUE self))
   struct itimerval timer;
 
   if (llrb_profiler.running) return Qfalse;
+  if (!llrb_profiler.sample_by_frame) {
+    llrb_profiler.sample_by_frame = st_init_numtable();
+  }
 
   sa.sa_sigaction = llrb_signal_handler;
   sa.sa_flags = SA_RESTART | SA_SIGINFO;
@@ -154,6 +223,22 @@ rb_jit_stop(RB_UNUSED_VAR(VALUE self))
 	sigaction(SIGPROF, &sa, NULL);
 
   return Qtrue;
+}
+
+static int
+llrb_gc_mark_i(st_data_t key, st_data_t val, st_data_t arg)
+{
+  VALUE frame = (VALUE)key;
+  rb_gc_mark(frame);
+  return ST_CONTINUE;
+}
+
+static void
+llrb_gc_mark(void *data)
+{
+  if (llrb_profiler.sample_by_frame) {
+    st_foreach(llrb_profiler.sample_by_frame, llrb_gc_mark_i, 0);
+  }
 }
 
 static void
@@ -191,5 +276,11 @@ Init_profiler(VALUE rb_mJIT)
   rb_define_singleton_method(rb_mJIT, "stop", RUBY_METHOD_FUNC(rb_jit_stop), 0);
 
   llrb_profiler.running = 0;
+  llrb_profiler.profile_times = 0;
+  llrb_profiler.sample_by_frame = 0;
+
+  gc_hook = Data_Wrap_Struct(rb_cObject, llrb_gc_mark, NULL, &llrb_profiler);
+  rb_global_variable(&gc_hook);
+
   pthread_atfork(llrb_atfork_prepare, llrb_atfork_parent, llrb_atfork_child);
 }
