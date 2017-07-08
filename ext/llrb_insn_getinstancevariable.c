584b85a2d8a7429479c5cff03f74f1a4d09785d1
Takashi Kokubun <takashikkbn@gmail.com>
2017-07-08 23:49:29 +0900

    Hand compile getinstancevariable

--- ext/llrb_insn_getinstancevariable.c
+++ ext/llrb_insn_getinstancevariable.c
@@ -1,18 +1,18 @@
 #include "cruby.h"
 
 static inline VALUE
-vm_getivar(VALUE obj, ID id, IC ic, struct rb_call_cache *cc, int is_attr)
+vm_getivar(VALUE obj, ID id, IC ic)
 {
   if (LIKELY(RB_TYPE_P(obj, T_OBJECT))) {
     VALUE val = Qundef;
-    if (LIKELY(is_attr ? cc->aux.index > 0 : ic->ic_serial == RCLASS_SERIAL(RBASIC(obj)->klass))) {
-      st_index_t index = !is_attr ? ic->ic_value.index : (cc->aux.index - 1);
+    if (LIKELY(ic->ic_serial == RCLASS_SERIAL(RBASIC(obj)->klass))) {
+      st_index_t index = ic->ic_value.index;
       if (LIKELY(index < ROBJECT_NUMIV(obj))) {
         val = ROBJECT_IVPTR(obj)[index];
       }
      undef_check:
       if (UNLIKELY(val == Qundef)) {
-        if (!is_attr && RTEST(ruby_verbose))
+        if (RTEST(ruby_verbose))
           rb_warning("instance variable %"PRIsVALUE" not initialized", QUOTE_ID(id));
         val = Qnil;
       }
@@ -27,25 +27,18 @@ vm_getivar(VALUE obj, ID id, IC ic, struct rb_call_cache *cc, int is_attr)
           if (index < ROBJECT_NUMIV(obj)) {
             val = ROBJECT_IVPTR(obj)[index];
           }
-          if (!is_attr) {
-            ic->ic_value.index = index;
-            ic->ic_serial = RCLASS_SERIAL(RBASIC(obj)->klass);
-          }
-          else { /* call_info */
-            cc->aux.index = (int)index + 1;
-          }
+          ic->ic_value.index = index;
+          ic->ic_serial = RCLASS_SERIAL(RBASIC(obj)->klass);
         }
       }
       goto undef_check;
     }
   }
-  if (is_attr)
-    return rb_attr_get(obj, id);
   return rb_ivar_get(obj, id);
 }
 
 VALUE
 llrb_insn_getinstancevariable(VALUE obj, ID id, VALUE ic_v)
 {
-  return vm_getivar(obj, id, (IC)ic_v, 0, 0);
+  return vm_getivar(obj, id, (IC)ic_v);
 }
