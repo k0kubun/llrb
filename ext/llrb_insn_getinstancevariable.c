9e6082e6309697588b7b1df299053e6d243e63cf
Takashi Kokubun <takashikkbn@gmail.com>
2017-07-12 22:07:52 +0900

    Split function to inline optimized part of getivar
    bm_ivar_while:::
    
    Before:
    Compile Time: 74.02ms
    Calculating -------------------------------------
                    Ruby      4.855  (± 0.0%) i/s -     25.000  in   5.149162s
                    LLRB      5.854  (± 0.0%) i/s -     30.000  in   5.124420s
    
    Comparison:
                    LLRB:        5.9 i/s
                    Ruby:        4.9 i/s - 1.21x  slower
    
    After:
    Compile Time: 86.33ms
    Calculating -------------------------------------
                    Ruby      4.853  (± 0.0%) i/s -     25.000  in   5.152017s
                    LLRB      6.321  (± 0.0%) i/s -     32.000  in   5.062765s
    
    Comparison:
                    LLRB:        6.3 i/s
                    Ruby:        4.9 i/s - 1.30x  slower
    
    optcarrot:::
    
    Before:
    $ be bin/optcarrot --benchmark examples/Lan_Master.nes
    fps: 43.38115561081955
    checksum: 59662
    
    After:
    $ be bin/optcarrot --benchmark examples/Lan_Master.nes
    fps: 44.584114915670554
    checksum: 59662

--- ext/llrb_insn_getinstancevariable.c
+++ ext/llrb_insn_getinstancevariable.c
@@ -1,5 +1,29 @@
 #include "cruby.h"
 
+static VALUE __attribute__ ((noinline))
+vm_getivar_else(VALUE obj, ID id, IC ic)
+{
+  st_data_t index;
+  struct st_table *iv_index_tbl = ROBJECT_IV_INDEX_TBL(obj);
+
+  VALUE val = Qundef;
+  if (iv_index_tbl) {
+    if (st_lookup(iv_index_tbl, id, &index)) {
+      if (index < ROBJECT_NUMIV(obj)) {
+        val = ROBJECT_IVPTR(obj)[index];
+      }
+      ic->ic_value.index = index;
+      ic->ic_serial = RCLASS_SERIAL(RBASIC(obj)->klass);
+    }
+  }
+  if (UNLIKELY(val == Qundef)) {
+    if (RTEST(ruby_verbose))
+      rb_warning("instance variable %"PRIsVALUE" not initialized", QUOTE_ID(id));
+    val = Qnil;
+  }
+  return val;
+}
+
 static inline VALUE
 vm_getivar(VALUE obj, ID id, IC ic)
 {
@@ -10,7 +34,6 @@ vm_getivar(VALUE obj, ID id, IC ic)
       if (LIKELY(index < ROBJECT_NUMIV(obj))) {
         val = ROBJECT_IVPTR(obj)[index];
       }
-     undef_check:
       if (UNLIKELY(val == Qundef)) {
         if (RTEST(ruby_verbose))
           rb_warning("instance variable %"PRIsVALUE" not initialized", QUOTE_ID(id));
@@ -19,19 +42,7 @@ vm_getivar(VALUE obj, ID id, IC ic)
       return val;
     }
     else {
-      st_data_t index;
-      struct st_table *iv_index_tbl = ROBJECT_IV_INDEX_TBL(obj);
-
-      if (iv_index_tbl) {
-        if (st_lookup(iv_index_tbl, id, &index)) {
-          if (index < ROBJECT_NUMIV(obj)) {
-            val = ROBJECT_IVPTR(obj)[index];
-          }
-          ic->ic_value.index = index;
-          ic->ic_serial = RCLASS_SERIAL(RBASIC(obj)->klass);
-        }
-      }
-      goto undef_check;
+      vm_getivar_else(obj, id, ic);
     }
   }
   return rb_ivar_get(obj, id);
