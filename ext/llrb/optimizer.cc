626c6fbd1f9ad7ce986215aab36c383f21eb40da
Takashi Kokubun <takashikkbn@gmail.com>
2017-09-17 15:54:45 +0900

    Inline 800

--- ext/llrb/optimizer.cc
+++ ext/llrb/optimizer.cc
@@ -246,7 +246,7 @@ RunModulePasses(llvm::Module *mod)
   llvm::PassManagerBuilder builder;
   builder.OptLevel  = 3;
   builder.SizeLevel = 0;
-  builder.Inliner = llvm::createFunctionInliningPass(700);
+  builder.Inliner = llvm::createFunctionInliningPass(800);
   builder.populateModulePassManager(mpm);
   if (0) PopulateModulePassManager(mpm);
 
